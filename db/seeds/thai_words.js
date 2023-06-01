0006091737d7fd216c647c6e81a5b13f81255897
Patt Sookmark <pijiya@sookmark.com>
2023-06-01 14:10:19 -0600

    Seed s3 datat

--- db/seeds/thai_words.js
+++ db/seeds/thai_words.js
@@ -3,29 +3,28 @@
  * @returns { Promise<void> }
  */
 exports.seed = async function (knex) {
-  // Deletes ALL existing entries
   await knex("thai_words").del();
   await knex("thai_words").insert([
     {
       id: 1,
-      img_url: "something for now",
-      audio_url: "something for now",
+      img_url: "https://images.unsplash.com/photo-1579613832125-5d34a13ffe2a",
+      audio_url: "https://d1dfsocfrnqlhe.cloudfront.net/apple-word.mp3",
       word: "apple",
-      categories: "general",
+      categories: "food",
     },
     {
       id: 2,
-      img_url: "nothing for now",
-      audio_url: "nothing for now",
+      img_url: "https://images.unsplash.com/photo-1528183429752-a97d0bf99b5a",
+      audio_url: "https://d1dfsocfrnqlhe.cloudfront.net/tree.mp3",
       word: "tree",
-      categories: "general",
+      categories: "Around Us",
     },
     {
       id: 3,
-      img_url: "nothing for now",
-      audio_url: "nothing for now",
+      img_url: "https://images.unsplash.com/photo-1489824904134-891ab64532f1",
+      audio_url: "https://d1dfsocfrnqlhe.cloudfront.net/car.mp3",
       word: "car",
-      categories: "general",
+      categories: "Vehicle",
     },
   ]);
 };
